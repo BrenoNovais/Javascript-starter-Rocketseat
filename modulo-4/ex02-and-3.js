c8db44b0a620137315fcac21409299a436550e3d
Breno <breno.novais@hotmail.com>
2020-08-18 08:55:04 -0300

    update

--- modulo-4/ex02-and-3.js
+++ modulo-4/ex02-and-3.js
@@ -2,18 +2,30 @@ btnElement2 = document.querySelector('#ex2, button.botao2');
 inputElement2 = document.querySelector('#ex2, input.input2');
 var ulElement = document.getElementById('result')
 
+
 btnElement2.onclick = function() {
-    axios.get('https://api.github.com/users/brenono/repos')
+
+    var createLi2 = document.createElement('li')
+    var textElement2 = document.createTextNode('Carregando . . .')
+    createLi2.appendChild(textElement2)
+    ulElement.appendChild(createLi2)
+
+
+    axios.get('https://api.github.com/users/' + inputElement2.value + '/repos')
     .then(function(response2){
+        document.getElementById('result').innerHTML = '<ul></ul>'
         var reps = response2.data
 
+        var cont = 0
+
         for (rep of reps){
+            cont += 1
             var createLi = document.createElement('li')
             var textElement = document.createTextNode(rep.name)
             createLi.appendChild(textElement)
             ulElement.appendChild(createLi)
-
             objectLi = ''
+            if (cont === 5) break
         }
         
 
@@ -24,14 +36,17 @@ btnElement2.onclick = function() {
         document.getElementById('result').innerHTML = div*/
     })
     .catch(function(error2){
-        console.warn(error2)
+        document.getElementById('result').innerHTML = '<ul></ul>'
+        var createLi2 = document.createElement('li')
+        var textElement2 = document.createTextNode('Usuario não existe!')
+        createLi2.appendChild(textElement2)
+        ulElement.appendChild(createLi2)
+
     });
 
     var idadeInputada2 = inputElement2.value
     console.log(idadeInputada2)
    
-
-
     inputElement2.value = ''
-
+    
 }
